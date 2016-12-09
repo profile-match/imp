b554ca07da9978c596f26c14cf50c6501e6a6d39
Your Name <you@example.com>
2016-12-09 15:02:48 +0100

    angular's bases

---
+++ angular/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<poste-form></poste-form>'
+})
+export class AppComponent { }
---
+++ angular/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { PosteFormComponent } from './recruteur/poste/poste-form.component';
+
+@NgModule({
+  imports:      [ BrowserModule,
+		  FormsModule ],
+  declarations: [ AppComponent,
+		  PosteFormComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
