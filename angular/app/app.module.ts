88dad36561b1f044e332df63642fcbb071447093
sopretty <jeanmougin.mat@gmail.com>
2016-12-18 23:34:44 +0100

    GitIgnore plus restreint

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
-
+import { PosteFormComponent } from './recruteur/poste/poste-form.component';
 
 @NgModule({
   imports:      [ BrowserModule,
 		  FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  PosteFormComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
