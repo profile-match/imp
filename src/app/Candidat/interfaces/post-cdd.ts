d6322fdce50bdb6b4037cdee6cad9fd8fe68c7af
Desmero <raphael.merkling@laposte.net>
2017-02-11 00:11:31 +0100

    Il y a les interfaces qui arrivent

---
+++ src/app/Candidat/interfaces/post-cdd.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by erwan on 17/01/2017.
+ */
+
+//TODO: A supprimer
+export interface poste {
+  intitule: string;
+  type_de_contrat: string;
+  lieu_de_travail: string;
+  date_de_publication: string;
+  resume : string;
+  reference: string;
+
+}
